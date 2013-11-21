f3777d0a2d5d8b36be8bbeb4d37b65068a82f40a
SupahNickie <nicholascase@live.com>
2013-11-20 21:54:33 -0800

    Last fix for speed optimization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.page(params[:page]).per_page(15)
+    @posts = Post.page(params[:page]).per_page(10)
 
     respond_to do |format|
       format.html # index.html.erb
